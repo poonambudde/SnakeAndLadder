818a5ba8973e192492cc0d99887fbc95b94cf2a2
poonambudde <buddepoonam123@gmail.com>
2022-02-12 19:31:53 +0530

    [Poonam] ADDED: Player checks for a Option NoPlay Ladder or Snake

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -6,12 +6,33 @@ namespace SnakeAndLadder
     {
         static void Main(string[] args)
         {
+            //Initializing Constants
+            const int LADDER = 1;
+            const int NOPLAY = 2;
+            const int SNAKE = 3;
 
             int position = 0;
             Random random = new Random();
 
             int noOnDie = random.Next(1, 7);
             Console.WriteLine($"No on die : {noOnDie}");
+
+            int options = random.Next(1, 4);
+
+            switch (options)
+            {
+                case LADDER:
+                    position += noOnDie;
+                    break;
+                case SNAKE:
+                    position -= noOnDie;
+                    break;
+                case NOPLAY:
+                    Console.WriteLine("POSITION " + position);
+                    break;
+            }
+            Console.WriteLine("POSITION " + position);
         }
+
     }
 }
